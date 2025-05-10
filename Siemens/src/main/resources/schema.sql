7e9114e4f3f3942b8228a11eb8a5f6f9b62a018e
TMarius23 <mariustrif0323@yahoo.com>
2025-05-10 15:55:28 +0300

    Delete Siemens directory

--- Siemens/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ITEM (
-                      ID BIGINT PRIMARY KEY,
-                      NAME VARCHAR(255),
-                      DESCRIPTION VARCHAR(255),
-                      STATUS VARCHAR(255),
-                      EMAIL VARCHAR(255)
-);
